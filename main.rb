7d6552c1d551f4ba6e962049ea6ce5baa594823e
bainjen <jennifer.fleming.bain@gmail.com>
2020-10-12 23:00:25 +0000

    implement switch_players and get_question_and_answer logic in Game class

--- main.rb
+++ main.rb
@@ -4,6 +4,27 @@ require './Game'
 
 
 player_one = Player.new("Pippa", 3)
+player_two = Player.new("Prairie", 3)
+game = Game.new(player_one, player_two)
+puts game.current_player.user_id
+puts game.current_opponent.user_id
+game.current_player.remove_life
+puts game.current_player.lives
+
+game.switch_players
+puts game.current_player.user_id
+puts game.current_opponent.user_id
+puts game.current_opponent.lives
+puts game.current_player.lives
+
+game.get_question_and_answer
+puts game.answer 
+puts game.question 
+
+game.get_question_and_answer
+puts game.answer 
+puts game.question 
+
 # puts player_one.user_id
 # player_copy = player_one
 # player_copy.user_id = "hM"
@@ -15,14 +36,15 @@ player_one = Player.new("Pippa", 3)
 # player_copy.remove_life
 # puts player_one.lives
 
-question_one = Question.new
-puts question_one.input_one
-question_one.generate_inputs
-puts question_one.input_one
-puts question_one.input_two
-puts question_one.calculate_answer
+# question_one = Question.new
+# puts question_one.input_one
+# question_one.generate_inputs
+# puts question_one.input_one
+# puts question_one.input_two
+# puts question_one.calculate_answer
 
-question_one.generate_inputs
-puts question_one.input_one
-puts question_one.input_two
-puts question_one.calculate_answer
+# question_one.generate_inputs
+# puts question_one.input_one
+# puts question_one.input_two
+# puts question_one.calculate_answer
+
